ae7158a5f018737917ad5dad26cd4ee5d69e7277
Sebastien Corbin <seb.corbin@gmail.com>
2015-10-08 13:24:13 +0200

    no message

--- templates/ucms/ucms-contrib-cart.tpl.php
+++ templates/ucms/ucms-contrib-cart.tpl.php
@@ -1,11 +1,16 @@
 <div id="ucms-cart" class="container-fluid">
   <div class="row">
     <h2><?php echo t("Your favorites"); ?></h2>
-    <?php foreach ($items as $nid => $item): ?>
-      <div class="ucms-cart-item col-md-6" data-nid="<?php echo render($nid); ?>">
-        <?php echo render($item); ?>
-      </div>
-    <?php endforeach; ?>
-    <a id="ucms-cart-trash" href="#"><?php echo t("Trash"); ?></a>
+
+    <div id="ucms-cart-list">
+      <?php foreach ($items as $nid => $item): ?>
+        <div class="ucms-cart-item col-md-6" data-nid="<?php echo render($nid); ?>">
+          <?php echo render($item); ?>
+        </div>
+      <?php endforeach; ?>
+    </div>
   </div>
+</div>
+<div id="ucms-cart-trash">
+  <span class="glyphicon glyphicon-trash"></span>
 </div>
