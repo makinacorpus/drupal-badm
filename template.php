08703644d51fda9b581475fc1e0937fa5ff470e2
Sebastien Corbin <seb.corbin@gmail.com>
2016-03-17 16:29:25 +0100

    Remove wrapping div wahen no list title

--- template.php
+++ template.php
@@ -796,3 +796,71 @@ function badm_preprocess_item_list(&$vars) {
     $vars['items'][] = $vars['empty_list'];
   }
 }
+
+/**
+ * Overrides theme_item_list().
+ */
+function badm_item_list(&$variables) {
+  $items = $variables['items'];
+  $title = $variables['title'];
+  $type = $variables['type'];
+  $attributes = $variables['attributes'];
+
+  // Only output the list container and title, if there are any list items.
+  // Check to see whether the block title exists before adding a header.
+  // Empty headers are not semantic and present accessibility challenges.
+  $output = '';
+  if (isset($title) && $title !== '') {
+    $output = '<div class="item-list">';
+    $output .= '<h3>' . $title . '</h3>';
+  }
+
+  if (!empty($items)) {
+    $output .= "<$type" . drupal_attributes($attributes) . '>';
+    $num_items = count($items);
+    $i = 0;
+    foreach ($items as $item) {
+      $attributes = [];
+      $children = [];
+      $data = '';
+      $i++;
+      if (is_array($item)) {
+        foreach ($item as $key => $value) {
+          if ($key == 'data') {
+            $data = $value;
+          }
+          elseif ($key == 'children') {
+            $children = $value;
+          }
+          else {
+            $attributes[$key] = $value;
+          }
+        }
+      }
+      else {
+        $data = $item;
+      }
+      if (count($children) > 0) {
+        // Render nested list.
+        $data .= theme_item_list([
+          'items'      => $children,
+          'title'      => NULL,
+          'type'       => $type,
+          'attributes' => $attributes,
+        ]);
+      }
+      if ($i == 1) {
+        $attributes['class'][] = 'first';
+      }
+      if ($i == $num_items) {
+        $attributes['class'][] = 'last';
+      }
+      $output .= '<li' . drupal_attributes($attributes) . '>' . $data . "</li>\n";
+    }
+    $output .= "</$type>";
+  }
+  if (isset($title) && $title !== '') {
+    $output .= '</div>';
+  }
+  return $output;
+}
